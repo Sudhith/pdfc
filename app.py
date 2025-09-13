ae1e926d4041390db3aaed68537baa42c9bd2b7b
Sudith <saisudhithdevadatta@gmail.com>
2025-09-14 02:07:32 +0530

    V4

--- app.py
+++ app.py
@@ -235,6 +235,8 @@ def resize_compress_image():
     os.rmdir(temp_dir)
 
     return safe_send_file(zip_path, "resized_images.zip")
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
